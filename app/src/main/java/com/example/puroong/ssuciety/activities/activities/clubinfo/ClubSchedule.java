9d8c673328a51aa4e205cda74c3c0d452af0ba47
tae <uhn61@naver.com>
2017-11-26 22:43:13 +0900

    no message

--- app/src/main/java/com/example/puroong/ssuciety/activities/activities/clubinfo/ClubSchedule.java
+++ app/src/main/java/com/example/puroong/ssuciety/activities/activities/clubinfo/ClubSchedule.java
@@ -1,15 +1,57 @@
 package com.example.puroong.ssuciety.activities.activities.clubinfo;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.DialogInterface;
+import android.graphics.Color;
 import android.os.Bundle;
+import android.support.v7.app.AlertDialog;
+import android.support.v7.app.AppCompatActivity;
 
 import com.example.puroong.ssuciety.R;
+import com.prolificinteractive.materialcalendarview.CalendarDay;
+import com.prolificinteractive.materialcalendarview.DayViewDecorator;
+import com.prolificinteractive.materialcalendarview.DayViewFacade;
+import com.prolificinteractive.materialcalendarview.MaterialCalendarView;
+import com.prolificinteractive.materialcalendarview.OnDateSelectedListener;
+import com.prolificinteractive.materialcalendarview.spans.DotSpan;
 
 public class ClubSchedule extends AppCompatActivity {
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_club_schedule);
+        setContentView(R.layout.activity_my_schedule);
+
+
+        MaterialCalendarView view = (MaterialCalendarView) findViewById(R.id.calendarView);
+
+        view.addDecorator(new DayViewDecorator() {
+            @Override
+            public boolean shouldDecorate(CalendarDay day) {
+                // TODO:
+                return true;
+            }
+
+            @Override
+            public void decorate(DayViewFacade view) {
+                view.addSpan(new DotSpan(5, Color.RED));
+            }
+        });
+
+        view.setOnDateChangedListener(new OnDateSelectedListener() {
+            @Override
+            public void onDateSelected(MaterialCalendarView widget, CalendarDay date, boolean selected) {
+                AlertDialog.Builder builder = new AlertDialog.Builder(getApplicationContext());
+
+                builder.setTitle("")
+                        .setItems(new String[]{}, new DialogInterface.OnClickListener() {
+                            @Override
+                            public void onClick(DialogInterface dialog, int which) {
+
+                            }
+                        });
+
+                AlertDialog dialog = builder.create();
+                dialog.show();
+            }
+        });
     }
 }
