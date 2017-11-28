b4bcba887e57816ba85db168d0c9542d2220ffd7
kongil <o_o0122@naver.com>
2017-11-28 12:00:03 +0900

    manifest에 추가해야함

---
+++ app/src/main/res/layout/profile_modified.java
@@ -0,0 +1,164 @@
+package com.example.puroong.ssuciety.activities;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.database.Cursor;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.Matrix;
+import android.media.ExifInterface;
+import android.net.Uri;
+import android.os.Bundle;
+import android.provider.MediaStore;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageButton;
+import android.widget.Spinner;
+import android.widget.Toast;
+
+import com.example.puroong.ssuciety.R;
+import com.example.puroong.ssuciety.activities.clublist.ClubListActivity;
+
+import java.util.ArrayList;
+
+public class profile_modified extends AppCompatActivity {
+    ImageButton imageButton;
+    private final int REQ_CAMERA_SELECT = 100;
+
+    EditText editText;
+    EditText editText2;
+    EditText editText3;
+    EditText editText4;
+
+    Spinner spinner;
+    String name;
+    String class1;
+    String entering_year;
+    int category;
+    String phone_num;
+
+    Button button;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_profile_modified);
+
+        imageButton = (ImageButton) findViewById(R.id.imageButton);
+        imageButton.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(Intent.ACTION_PICK);
+                intent.setType(android.provider.MediaStore.Images.Media.CONTENT_TYPE);
+                startActivityForResult(intent, REQ_CAMERA_SELECT);
+            }
+        });
+
+
+
+        editText = (EditText)findViewById(R.id.editText);
+        editText2 = (EditText)findViewById(R.id.editText2);
+        editText3 = (EditText)findViewById(R.id.editText3);
+        editText4 = (EditText)findViewById(R.id.editText4);
+
+        final ArrayList<String> list = new ArrayList<>();
+        list.add("1.연대사업");
+        list.add("2.연행예술");
+        list.add("3.종교");
+        list.add("4.창작전시");
+        list.add("5.체육");
+        list.add("6.학술");
+        spinner = (Spinner)findViewById(R.id.spinner);
+        ArrayAdapter sadapter;
+        sadapter = new ArrayAdapter(this, R.layout.support_simple_spinner_dropdown_item,list);
+        spinner.setAdapter(sadapter);
+        spinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> adapterView, View view, int i, long l) {
+                Toast.makeText(profile_modified.this,""+i,Toast.LENGTH_LONG).show();
+                category = i+1;
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> adapterView) {
+
+            }
+        });
+
+        final Intent intent = new Intent(getApplicationContext(), ClubListActivity.class);
+
+        button = (Button) findViewById(R.id.button);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                name = editText.getText().toString();
+                class1 = editText2.getText().toString();
+                entering_year = editText3.getText().toString();
+                phone_num = editText4.getText().toString();
+                startActivity(intent);
+                finish();
+            }
+        });
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if(requestCode == 100){
+            if(resultCode == Activity.RESULT_OK){
+                try{
+                    Uri imgUri = data.getData();
+                    String imagePath = getRealPathFromURI(imgUri); // path 경로
+                    ExifInterface exif = null;
+                    int exifOrientation = exif.getAttributeInt(ExifInterface.TAG_ORIENTATION, ExifInterface.ORIENTATION_NORMAL);
+                    int exifDegree = exifOrientationToDegrees(exifOrientation);
+
+                    Bitmap bitmap = BitmapFactory.decodeFile(imagePath);//경로를 통해 비트맵으로 전환 
+                    imageButton.setImageBitmap(rotate(bitmap, exifDegree));//이미지 뷰에 비트맵 넣기
+
+                }catch(Exception e){
+                    Toast.makeText(getBaseContext(), "사진불러오기 실패", Toast.LENGTH_LONG).show();
+                }
+            }
+        }
+        super.onActivityResult(requestCode, resultCode, data);
+    }
+
+    private int exifOrientationToDegrees(int exifOrientation) {
+        if (exifOrientation == ExifInterface.ORIENTATION_ROTATE_90) {
+            return 90;
+        } else if (exifOrientation == ExifInterface.ORIENTATION_ROTATE_180) {
+            return 180;
+        } else if (exifOrientation == ExifInterface.ORIENTATION_ROTATE_270) {
+            return 270;
+        }
+        return 0;
+
+    }
+    public Bitmap rotate(Bitmap src, float degree) {
+
+        // Matrix 객체 생성
+        Matrix matrix = new Matrix();
+        // 회전 각도 셋팅
+        matrix.postRotate(degree);
+        // 이미지와 Matrix 를 셋팅해서 Bitmap 객체 생성
+        return Bitmap.createBitmap(src, 0, 0, src.getWidth(),
+                src.getHeight(), matrix, true);
+    }
+
+    private String getRealPathFromURI(Uri imgUri) {
+        int column_index=0;
+        String[] proj = {MediaStore.Images.Media.DATA};
+        Cursor cursor = getContentResolver().query(imgUri, proj, null, null, null);
+        if(cursor.moveToFirst()){
+            column_index = cursor.getColumnIndexOrThrow(MediaStore.Images.Media.DATA);
+        }
+
+        return cursor.getString(column_index);
+
+
+    }
+}
